c96fb5cbedcac474f068f44b189c10e4029dbcd4
김현진 <flapper.jinn@gmail.com>
2020-05-23 23:09:32 +0900

    update read.me

---
+++ src/main/java/org/codezen/jhmybatis/service/PredictedException.java
@@ -0,0 +1,7 @@
+package org.codezen.jhmybatis.service;
+
+public class PredictedException extends Exception {
+    public PredictedException(String errorMessage) {
+        super(errorMessage);
+    }
+}

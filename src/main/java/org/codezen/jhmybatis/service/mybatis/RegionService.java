0042e7558883df2bf58150c57be1901337339570
김현진 <flapper.jinn@gmail.com>
2020-05-23 17:07:49 +0900

    Applied transaction codes & some messages

--- src/main/java/org/codezen/jhmybatis/service/mybatis/RegionService.java
+++ src/main/java/org/codezen/jhmybatis/service/mybatis/RegionService.java
@@ -4,17 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Service Interface for managing {@link Region}.
  */
 public interface RegionService {
-
-    /**
-     * Save a region.
-     *
-     * @param region the entity to save.
-     * @return the persisted entity.
-     */
-    void save(HashMap<String, Object> map);
 
     /**
      * Get all the regions.
@@ -26,19 +17,22 @@ public interface RegionService {
     /**
      * Get the "id" region.
      *
-     * @param id the id of the entity.
+     * @param map the id of the entity.
      * @return the entity.
      */
-    CMap<String, Object> findOne(HashMap<String, Object> map);
+    HashMap<String, Object> findOne(HashMap<String, Object> map);
 
     /**
      * Delete the "id" region.
      *
-     * @param id the id of the entity.
+     * @param map the id of the entity.
      */
     void delete(HashMap<String, Object> map);
 
     void insert(HashMap<String, Object> map);
 
     void update(HashMap<String, Object> map);
+
+    int ifExists(HashMap<String,Object> map);
+
 }
